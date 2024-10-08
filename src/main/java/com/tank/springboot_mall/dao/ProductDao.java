fd89c87fac37ccd4b5a48fa6bdc3c80fecd3c1f7
Tank-LIN <aduv5599@gmail.com>
2024-10-08 20:48:26 +0800

    商品功能 - 實作查詢商品列表(基礎實作)

--- src/main/java/com/tank/springboot_mall/dao/ProductDao.java
+++ src/main/java/com/tank/springboot_mall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.tank.springboot_mall.dao;
 import com.tank.springboot_mall.dto.ProductRequest;
 import com.tank.springboot_mall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/tank/springboot_mall/service/ProductService.java
+++ src/main/java/com/tank/springboot_mall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.tank.springboot_mall.service;
 import com.tank.springboot_mall.dto.ProductRequest;
 import com.tank.springboot_mall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
