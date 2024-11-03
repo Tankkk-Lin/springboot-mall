3e5280b36bb041ff98f58a022e2b5561de029dc0
Tank-LIN <aduv5599@gmail.com>
2024-11-03 13:59:20 +0800

    商品功能 - 實作查詢商品列表(分頁)

--- src/main/java/com/tank/springboot_mall/controller/ProductController.java
+++ src/main/java/com/tank/springboot_mall/controller/ProductController.java
@@ -8,11 +8,15 @@ import com.tank.springboot_mall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
+@Validated      // 4-13 驗證請求註解
 @RestController
 public class ProductController {
 
@@ -27,13 +31,19 @@ public class ProductController {
 
             //  排序 Sorting
             @RequestParam(defaultValue = "created_date") String orderBy,       //  根據什麼欄位排序,    4-7 defaultValue使用
-            @RequestParam(defaultValue = "desc") String sort               //  要使用升序活是降序
+            @RequestParam(defaultValue = "desc") String sort,               //  要使用升序活是降序
+
+            //  分頁 Pagination
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,    //  要取得幾筆商品數據
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset   //  要跳過幾筆商品數據
     ) {
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
